afce4b908435c2d73df6bfa7ca2c30fcfab35023
Tina Shi <tshi@umail.ucsb.edu>
2021-03-02 03:42:36 -0800

    how to or operation with nonce

--- blockchain.py
+++ blockchain.py
@@ -9,19 +9,35 @@ class Operation:
 		self.val = val
 
 class Block:
-	def __init__(self, op=None, prev=None):
+	def __init__(self, op, prev):
 		self.op = op
 		self.prev = prev
-		self.nonce = self.calc_nounce()
+		self.nonce = self.calc_nonce()
 
-	def difficulty_check(self, block_hash):
+	def difficulty_check(self, nonce):
+		hasher = sha256()
+		hasher.update()
 		dig = block_hash[-1]
 		for i in range(3):
 			if dig == str(i):
 				return True
 		return False
 
-	def calc_nounce(self):
+	def calc_nonce_helper(self, nonce, poss_chars, max_len):
+		if len(nonce) == max_len:
+			return None
+
+		for c in poss_chars:
+			n = nonce + c
+			if self.difficulty_check(n):
+				return n
+		
+		for c in poss_chars:
+			n = self.calc_nonce_helper(nonce + c, poss_chars, max_len)
+			if n is not None:
+				return n
+
+	def calc_nonce(self, max_len=10):
 		poss_chars = (" !\"#$%&'()*+-,-./"
 			"0123456789"
 			":;<=>?@"
@@ -30,8 +46,7 @@ class Block:
 			"abcdefghijklmnopqrstuvwxyz"
 			"{|}~")
 
-		nounce_len = 1
-		# TODO
+		return self.calc_nonce_helper("", poss_chars, max_len)
 
 class Blockchain:
 	def __init__(self):
