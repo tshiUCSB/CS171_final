923730f3101c94fd738850a6e2225b6ee3646d77
Tina Shi <tshi@umail.ucsb.edu>
2021-03-02 03:43:35 -0800

    changed node comment

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-# creation of node that is both server and client
+# creation of node that servers operation requests from clients
 # to participate in paxos in maintaining blockchain
 
 import json
