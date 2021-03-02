44148a571118311645c6660ab8aa1da3ae09b818
Tina Shi <tshi@umail.ucsb.edu>
2021-03-02 08:27:53 -0800

    added locks to some data structures

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 # definition of key-store database
 
+from threading import Lock
+
 class Database:
 	def __init__(self):
 		self.store = {}
+		self.lock = Lock()
 
 	def __str__(self):
 		return str(self.store)
@@ -21,7 +24,9 @@ class Database:
 
 	def put(self, key, val):
 		# val format {"phone_number": "111-222-3333"}
-		self.store[key] = val
+		with self.lock:
+			self.store[key] = val
 
 	def get(self, key):
-		return self.store[key]
+		with self.lock:
+			return self.store[key]
--- queue.py
+++ queue.py
@@ -1,34 +1,41 @@
 # squash data structure for keeping track of operation ballots
 
 import heapq
+from threading import Lock
 
 class Queue:
 	def __init__(self):
 		self.heap = []
 		self.hash_table = {}
+		self.lock = Lock()
 
 	def push(self, ballot):
-		heapq.heappush(self.heap, ballot.num)
-		key = str(ballot.num)
-		self.hash_table[key] = ballot
+		with self.lock:
+			heapq.heappush(self.heap, ballot.num)
+			key = str(ballot.num)
+			self.hash_table[key] = ballot
 
 	def pop(self):
-		ballot_num = heapq.heappop(self.heap)
-		key = str(ballot_num)
-		ballot = self.hash_table.pop(key)
-		return ballot
+		with self.lock:
+			ballot_num = heapq.heappop(self.heap)
+			key = str(ballot_num)
+			ballot = self.hash_table.pop(key)
+			return ballot
 
 	def top(self):
-		ballot_num = self.heap[0]
-		key = str(ballot_num)
-		return self.hash_table[key]
+		with self.lock:
+			ballot_num = self.heap[0]
+			key = str(ballot_num)
+			return self.hash_table[key]
 
 	def accept(self, ballot_num, pid):
-		key = str(ballot_num)
-		self.hash_table[key].acceptance.add(pid)
-		return get_accept_count(ballot_num)
+		with self.lock:
+			key = str(ballot_num)
+			self.hash_table[key].acceptance.add(pid)
+			return get_accept_count(ballot_num)
 
 	def get_accept_count(self, ballot_num):
-		key = str(ballot_num)
-		return len(self.hash_table[key].acceptance)
+		with self.lock:
+			key = str(ballot_num)
+			return len(self.hash_table[key].acceptance)
 
